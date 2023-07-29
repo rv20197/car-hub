bfa63fa51e4131f844b9592591434ce00bdc11da
Vatsal Rajgor <rv20197@gmail.com>
2023-07-30 01:07:00 +0530

    Added Scroll to Explore Car Button

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,15 +1,15 @@
-"use client"
-import React from 'react'
-import CustomButton from '../CustomButton/CustomButton'
-import Image from 'next/image'
+'use client';
+import CustomButton from '../CustomButton/CustomButton';
+import Image from 'next/image';
 
-
-type Props = {}
-
-const Hero = (props: Props) => {
+const Hero = () => {
 	const handleScroll = () => {
-
-	}
+		const carCatalogue = document.getElementById('discover');
+		window.scrollTo({
+			top: carCatalogue?.offsetTop,
+			behavior: 'smooth'
+		});
+	};
 
 	return (
 		<div className='hero'>
@@ -18,22 +18,32 @@ const Hero = (props: Props) => {
 					Find, book or rent a car -- quickly and easily!
 				</h1>
 				<p className='hero__subtitle'>
-					Streamline your car rental experience with our effortless booking process.
+					Streamline your car rental experience with our effortless booking
+					process.
 				</p>
 
-				<CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10"
+				<CustomButton
+					title='Explore Cars'
+					containerStyles='bg-primary-blue text-white rounded-full mt-10'
 					handleClick={handleScroll}
 				/>
 			</div>
 
 			<div className='hero__image-container'>
 				<div className='hero__image'>
-					<Image src='/hero.png' alt={'hero'} fill className='object-contain' priority={true} sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"/>
+					<Image
+						src='/hero.png'
+						alt={'hero'}
+						fill
+						className='object-contain'
+						priority={true}
+						sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+					/>
 				</div>
-				<div className='hero__image-overlay'/>
+				<div className='hero__image-overlay' />
 			</div>
 		</div>
-	)
-}
+	);
+};
 
-export default Hero
+export default Hero;
