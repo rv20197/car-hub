90d9426ca8a09d29122dd1a53169cfe67678b1f7
Vatsal Rajgor <rv20197@gmail.com>
2023-07-30 00:01:55 +0530

    Added Custom Filter

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { CarCard, CustomFilter, Hero, SearchBar } from '../components';
+import { fuels, yearsOfProduction } from '../constants';
 import { FilterInterface, fetchCars } from '../utils';
 
-export default async function Home(searchParams: FilterInterface) {
+export default async function Home(searchFilter: FilterInterface) {
+	const { searchParams } = searchFilter;
 	const allCars = await fetchCars({
 		manufacturer: searchParams.manufacturer || '',
 		year: searchParams.year || 2022,
@@ -24,8 +26,8 @@ export default async function Home(searchParams: FilterInterface) {
 				<div className='home__filters'>
 					<SearchBar />
 					<div className='home__filter-container'>
-						<CustomFilter title='fuel' />
-						<CustomFilter title='year' />
+						<CustomFilter title='fuel' options={fuels} />
+						<CustomFilter title='year' options={yearsOfProduction} />
 					</div>
 				</div>
 
--- utils/index.ts
+++ utils/index.ts
@@ -55,3 +55,12 @@ export const generateCarImageUrl = (car: carProps['car'], angle?: string) => {
 
 	return `${url}`;
 };
+
+export const updateSearchParams = (title: string, value: string) => {
+	const searchParams = new URLSearchParams(window.location.search);
+	searchParams.set(title, value);
+
+	const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+	return newPathname;
+};
