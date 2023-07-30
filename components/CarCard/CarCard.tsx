0672386d84e8af92b26e164bda68bb530f1afaf1
Vatsal Rajgor <rv20197@gmail.com>
2023-07-30 16:21:10 +0530

    Removed Rental Pricing

--- components/CarCard/CarCard.tsx
+++ components/CarCard/CarCard.tsx
@@ -36,12 +36,6 @@ const CarCard = ({ car }: carProps) => {
 				</h2>
 			</div>
 
-			<p className='flex mt-6 text-[32px] font-extrabold'>
-				<span className='self-start text-[14px] font-semibold'>$</span>
-				{carRent}
-				<span className='self-end text-[14px] font-medium'>/day</span>
-			</p>
-
 			<div className='relative w-full h-40 my-3 object-contain'>
 				<Image
 					src={generateCarImageUrl(car)}
